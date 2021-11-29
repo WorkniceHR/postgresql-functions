dc6e1c3e5e32117425cd1889c8a4b2ddbf7bd796
Haydn Ewers <27211+haydn@users.noreply.github.com>
2021-11-29 17:30:43 +1100

    Create is_valid_acn.sql

---
+++ is_valid_acn.sql
@@ -0,0 +1,34 @@
+/*
+
+Checks if the given text is a valid ABN (Australian Company Number) using the formula provided by ASIC:
+
+https://asic.gov.au/for-business/registering-a-company/steps-to-register-a-company/australian-company-numbers/australian-company-number-digit-check/
+
+Leading and trailing whitespace is allowed as is any whitespace between digits.
+
+*/
+
+create function is_valid_acn(text) returns boolean
+as $$
+  declare
+    digits text[] := array[]::text[];
+    total int := 0;
+    i int := 0;
+  begin
+    RAISE NOTICE 'start: %', regexp_replace($1, '\s', '', 'g');
+    if $1 ~ '^(\s*\d\s*){9}$' then
+      RAISE NOTICE 'legit';
+      digits := string_to_array(regexp_replace($1, '\s', '', 'g'), null);
+      for i in 1..8 loop
+        total := total + cast(digits[i] as int) * (9 - i);
+      end loop;
+      RAISE NOTICE 'total: %', total;
+      return total % 10 = 10 - cast(digits[9] as int);
+    else
+      return false;
+    end if;
+  end;
+$$
+language 'plpgsql'
+immutable
+returns null on null input;
