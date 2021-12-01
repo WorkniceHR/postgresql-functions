af747688ce830af9e1c2b62578bcef3173bc83ce
Haydn Ewers <27211+haydn@users.noreply.github.com>
2021-12-01 18:38:45 +1100

    Update is_valid_abn.sql

--- is_valid_abn.sql
+++ is_valid_abn.sql
@@ -8,23 +8,26 @@ Leading and trailing whitespace is allowed as is any whitespace between digits.
 
 */
 
-CREATE FUNCTION is_valid_abn(text) RETURNS boolean
-AS $$
-  select sum(multiplier * cast(value as int)) % 89 = 0
-  from unnest(
-    array[10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
-    string_to_array(
-      cast((cast((
-        case
-          when $1 ~ '^(\s*\d\s*){11}$'
-            then regexp_replace($1, '\s', '', 'g')
-          else '11111111111'
-        end
-      ) as bigint) - 10000000000) as text),
-      NULL
-    )
-  ) as x(multiplier, value);
+create function is_valid_abn(text) returns boolean
+as $$
+  declare
+    digits text[] := array[]::text[];
+    weighting_factors int[] := array[10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+    total int := 0;
+    i int := 0;
+  begin
+    if $1 ~ '^(\s*\d\s*){11}$' then
+      digits := string_to_array(regexp_replace($1, '\s', '', 'g'), null);
+      digits[1] := cast(cast(digits[1] as int) - 1 as text);
+      for i in 1..11 loop
+        total := total + cast(digits[i] as int) * weighting_factors[i];
+      end loop;
+      return total % 89 = 0;
+    else
+      return false;
+    end if;
+  end;
 $$
-LANGUAGE SQL
-IMMUTABLE
-RETURNS NULL ON NULL INPUT;
+language 'plpgsql'
+immutable
+returns null on null input;
